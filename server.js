e90a32754193738383c6b382268514622a5b1a6e
Krrish Singh <krrishjgd@gmail.com>
2026-04-06 19:38:44 +0530

    serve frontend

--- server.js
+++ server.js
@@ -157,6 +157,14 @@ app.get('/logout', (req, res) => {
     });
 });
 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
