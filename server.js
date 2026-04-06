cbe8bc0764684d10e1973dad37a0ac5e24960256
Krrish Singh <krrishjgd@gmail.com>
2026-04-06 21:06:10 +0530

    fix port binding for render

--- server.js
+++ server.js
@@ -162,12 +162,12 @@ const PORT = process.env.PORT || 3000;
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port " + PORT);
 });
