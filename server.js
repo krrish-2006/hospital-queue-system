309699b318c4506d01d0ffd09b33f55279c4d5ed
Krrish Singh <krrishjgd@gmail.com>
2026-04-07 09:39:08 +0530

    fix redirect uri

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ mongoose.connect(process.env.MONGO_URI)
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://hospital-queue-system-yp10.onrender.com/auth/google/callback"
+    callbackURL: "https://hospital-queue-system-ypl0.onrender.com/auth/google/callback"
 },
 (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
